db77b551663339d5a26c985da3293ee6c6146b26
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2020-05-23 12:29:42 +0800

    Declare Client as publix

--- Sources/MatrixClient/MatrixClient.swift
+++ Sources/MatrixClient/MatrixClient.swift
@@ -1,7 +1,18 @@
 import Foundation
+import Combine
 import NetworkDispatcher
 
-struct MatrixClient {
+public struct MatrixClient {
     private let dispatcher: Dispatcher
     private let baseURL: URL
+}
+
+public extension MatrixClient {
+    func publicRooms() -> AnyPublisher<[Room], Error> {
+        let request = URLRequest(url: baseURL.appendingPathComponent("_matrix/client/r0/publicRooms"))
+        
+        return dispatcher.dispatch(request, JSONDecoder())
+            .map(\.value)
+            .eraseToAnyPublisher()
+    }
 }
